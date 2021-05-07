418d356f359f0a9eed1b06e17c02cae0e76edf6b
igrolom <59706892+igrolom@users.noreply.github.com>
2021-05-07 16:48:54 +0300

    change

--- server.js
+++ server.js
@@ -81,7 +81,8 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   //   res.send('this is working');
-  res.send(database.users);
+  // res.send(database.users);
+  res.send('it is working');
 });
 
 // SIGNING POST
